3975f2348c9825255ba09c8eb950869c492140a6
Piero <piero071198@gmail.com>
2020-05-10 11:16:35 +0200

    Añadida la clase PruebaGit

---
+++ src/ejerciciogitnetbeanspiero/EjercicioGitNetbeansPiero.java
@@ -0,0 +1,14 @@
+
+package ejerciciogitnetbeanspiero;
+
+
+public class EjercicioGitNetbeansPiero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/ejerciciogitnetbeanspiero/PruebaGit.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogitnetbeanspiero;
+
+/**
+ *
+ * @author piero
+ */
+public class PruebaGit {
+    public int numero =5;
+}
