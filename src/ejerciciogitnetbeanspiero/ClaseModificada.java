5e142b3bdcac5ce2138f31000c7db31d8480d7b0
Piero <piero071198@gmail.com>
2020-05-12 13:55:56 +0200

    Añadida la clase ClaseModificada

---
+++ src/ejerciciogitnetbeanspiero/ClaseModificada.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogitnetbeanspiero;
+
+/**
+ *
+ * @author piero
+ */
+public class ClaseModificada {
+    public static String nombre;
+    
+}
