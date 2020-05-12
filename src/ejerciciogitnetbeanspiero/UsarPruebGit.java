53fe569c96ea4847422c8d2fd53cd2cf1891e8f7
piero071198 <65229639+piero071198@users.noreply.github.com>
2020-05-12 14:03:58 +0200

    Update UsarPruebGit.java

--- src/ejerciciogitnetbeanspiero/UsarPruebGit.java
+++ src/ejerciciogitnetbeanspiero/UsarPruebGit.java
@@ -11,6 +11,6 @@ package ejerciciogitnetbeanspiero;
  */
 public class UsarPruebGit {
     public static void main(String[] args) {
-        System.out.println("Hola mundo desde Git");
+        System.out.println("Hola mundo desde GitHub");
     }
 }
